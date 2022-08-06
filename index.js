f23274f5ecd8206809cb7b62b911cd3d09129294
Rahat Siddique <rahatsiddique2717@gmail.com>
2022-08-06 20:01:33 +0600

    finished

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
  */
 
 // Dependencies
-const mathLibrary = require('math');
-const quotesLibrary = require('quotes');
+const mathLibrary = require('./lib/math');
+const quotesLibrary = require('./lib/quotes');
 
 // App object - Module scaffolding
 const app = {};
@@ -16,3 +16,30 @@ const app = {};
 app.config = {
     timeBetweenQuotes : 1000
 }
+
+// Function that prints a random quote
+app.printAQuote = function printAQuote(){
+    // Get all the quotes
+    const allQuotes = quotesLibrary.allQuotes();
+
+    // Get the length of the quotes
+    const numberOfQuotes = allQuotes.length;
+
+    // Pick a random number between 1 and the number of quotes
+    const randomNumber = mathLibrary.getRandomNumber(1, numberOfQuotes);
+
+    // Get the quote at that position (minus one)
+    const selectedQuote = allQuotes[randomNumber - 1];
+
+    // Print the quote to the console
+    console.log(selectedQuote);
+};
+
+// Function that loops indefinitely, calling the printAQuote function as it goes
+app.indefiniteLoop = function indefiniteLoop(){
+    // Create the interval, using the config variable defined above
+    setInterval(app.printAQuote, app.config.timeBetweenQuotes);
+};
+
+// Invoke the loop
+app.indefiniteLoop();
